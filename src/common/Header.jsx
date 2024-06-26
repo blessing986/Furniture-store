048c865acf4a47a27eea5612ee573eb2b45ad722
Blessing U <blessing986@users.noreply.github.com>
2024-06-26 11:50:17 +0100

    feat: implemented header component.

---
+++ src/common/Header.jsx
@@ -0,0 +1,80 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { navbar } from "../data/Data";
+import { HiOutlineHeart, HiOutlineUser } from "react-icons/hi";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import Sidebar from "../common/Sidebar";
+
+const Header = () => {
+  const [sticky, setSticky] = useState(false);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setSticky(window.scrollY > 0);
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  return (
+    <div>
+      <div
+        className={`${sticky ? "sticky header py-4 top-0 z-50 shadow-xl" : ""}`}
+      >
+        <div className="w-10/12 m-auto flex flex-wrap justify-between items-center">
+          <div>
+            <div className="logo">miniture</div>
+          </div>
+
+          <div className="md:flex flex-wrap text-base py-3">
+            {navbar.map((val, key) => (
+              <div key={key} className="mr-5">
+                <Link
+                  className="active link-hover transition-all"
+                  to={val.path}
+                >
+                  {val.nav}
+                </Link>
+              </div>
+            ))}
+          </div>
+
+          <li className="flex flex-wrap">
+            <Link className="mr-5 text-2xl">
+              <HiOutlineHeart />
+            </Link>
+
+            <Link className="mr-5 text-2xl">
+              <HiOutlineUser />
+            </Link>
+
+            <Link className="relative mr-5 text-2xl" onClick={toggleSidebar}>
+              <MdOutlineShoppingBag />
+
+              <div className="items_count">
+                <span className="text-white">0</span>
+              </div>
+            </Link>
+          </li>
+        </div>
+      </div>
+
+      <div>
+        <Sidebar
+          isSidebarOpen={isSidebarOpen}
+          closeSidebar={() => toggleSidebar()}
+        />
+      </div>
+    </div>
+  );
+};
+export default Header;
